19ee85d6f0679882deffb8df23b9b8ee1c313c21
Lilian <lilian.barbierlaffitte@gmail.com>
2023-07-07 21:30:06 +0200

    added character controller and animation

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class CharacterController : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private bool isStopped;
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private float speed;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+    private void OnEnable()
+    {
+        playerInput.Redomino.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Redomino.Disable();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = playerInput.Redomino.Movement.ReadValue<Vector2>();
+
+        if (!isStopped)
+        {
+
+            if (movement.x != 0)
+            {
+                animator.SetTrigger("MoveRight");
+                spriteRenderer.flipX = movement.x < 0;
+            }
+            else if (movement.y < 0)
+            {
+                animator.SetTrigger("MoveDown");
+            }
+            else if (movement.y > 0)
+            {
+                animator.SetTrigger("MoveUp");
+            }
+
+            rigidbody.MovePosition(rigidbody.position + movement * speed * Time.deltaTime);
+        }
+    }
+}
