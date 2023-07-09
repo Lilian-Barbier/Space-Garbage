13f1f2bb9ed110f106544f902c009c61ad3a3f72
Evan <evan.lajusticia@gmail.com>
2023-07-09 16:39:06 +0200

    made score into a prefab, added base for main menu, added gameover to levels

---
+++ Assets/Scripts/Menu/ScrollingMosaic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingMosaic : MonoBehaviour
+{
+
+    [SerializeField] private RawImage image;
+    [SerializeField] private float xSpeed = 1f;
+    [SerializeField] private float ySpeed = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.uvRect = new Rect(image.uvRect.position + new Vector2(xSpeed, ySpeed) * Time.deltaTime, image.uvRect.size);
+    }
+}
