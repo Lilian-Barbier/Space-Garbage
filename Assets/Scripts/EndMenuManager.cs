91ffc57ebd21c31f51524002e926ceba241a46df
LePlacard <58773506+Lilian-Barbier@users.noreply.github.com>
2023-07-09 11:48:08 +0200

    added gameover menu in prefab HUD

---
+++ Assets/Scripts/EndMenuManager.cs
@@ -0,0 +1,36 @@
+using Models;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Utils;
+
+public class EndMenuManager : MonoBehaviour
+{
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    public void EndLevel(int score)
+    {
+        GetComponent<CanvasGroup>().alpha = 1;
+        transform.Find("FinalScore").GetComponent<TextMeshProUGUI>().text = "SCORE : " + score;
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+}
