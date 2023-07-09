1a2a646a89609a06cfad7c0ef3001e25ae0a956b
Evan <evan.lajusticia@gmail.com>
2023-07-09 18:41:06 +0200

    added bobbing to main menu button

--- Assets/Scripts/Menu/ButtonBobbing.cs
+++ Assets/Scripts/Menu/ButtonBobbing.cs
@@ -7,6 +7,7 @@ public class ButtonBobbing : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time) * 0.1f + 0.5f, transform.position.z);
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(rectTransform.anchoredPosition.x, Mathf.Sin(Time.time * 4f) * 10f - 3f, 0);
     }
 }
